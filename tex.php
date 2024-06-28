3c8606623dba3f3d562307cdc78b6700a7c352d9
Wavyspla9980 <tr0lich@yandex.ru>
2024-06-28 14:23:07 +0300

    tex.php

---
+++ tex.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<button onclick="document.location='https://www.youtube.com/watch?v=HIcSWuKMwOw&t=4s'">click</button>
+</body>
+</html>
